ac2083e9824bc77613d132abe8cb1fc1cb2a21f0
ronakgarg-byte <gargronak321@gmail.com>
2026-09-20 09:39:25 +0530

    Time: 84 ms (19.63%) | Memory: 111.4 MB (46.34%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -16,3 +16,23 @@ public:
         return false;
     }
 };
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         for(int i = 0; i < n; i++) {
+
+//             for(int j = i + 1; j < n; j++) {
+
+//                 if(nums[i] == nums[j]) {
+//                     return true;
+//                 }
+
+//             }
+//         }
+
+//         return false;
+//     }
+// };
